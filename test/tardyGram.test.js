fc10c7345cbb3a9069109539ce831daacc6b81ba
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-07-22 17:55:37 -0700

    we can GET all tardyGrams

--- test/tardyGram.test.js
+++ test/tardyGram.test.js
@@ -5,6 +5,7 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const User = require('../lib/models/User');
+const Tardygram = require('../lib/models/Tardygram');
 const ensureAuth = require('../lib/middleware/ensure-auth');
 
 describe('user routes', () => {
@@ -27,6 +28,16 @@ describe('user routes', () => {
       .post('/api/v1/auth/signin')
       .send({ email: user.email, password: 'testtest' });
   });
+
+  let tardyGram = null;
+  beforeEach(async() => {
+    tardyGram = JSON.parse(JSON.stringify(await Tardygram.create({ 
+      photoUrl: 'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      caption: 'its a cat',
+      tags: ['#yellow', '#tabby'],
+      user: user._id
+    })));
+  });
   
   afterAll(() => {
     return mongoose.connection.close();
@@ -41,7 +52,6 @@ describe('user routes', () => {
         tags: ['#yellow', '#tabby']
       })
       .then(res => {
-        console.log(res.body, 'res.body')
         expect(res.body).toEqual({ 
           _id: expect.any(String),
           photoUrl: 'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
@@ -53,6 +63,22 @@ describe('user routes', () => {
             __v: 0
           },
           __v: 0
+        });
+      });
+  });
+
+  it('GET all tardyGrams', async() => {
+    return request(app)
+      .get('/api/v1/tardyGrams')
+      .then(res => {
+        expect(res.body).toContainEqual({ 
+          _id: expect.any(String),
+          photoUrl: tardyGram.photoUrl,
+          caption: tardyGram.caption,
+          tags: tardyGram.tags,
+          user: {
+            _id: expect.any(String)
+          }
         });
       });
   });
